3860d7e0e9a9656630a787ae668f84310e5b54f1
Renaud <renaudfradin@gmail.com>
2020-05-13 17:19:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready" , () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message" , msg => {
+    if (msg.content === "hello") {
+        msg.reply("BONJOUR MAITRE");
+    }else if (msg.content === "batar") {
+        msg.delete(msg);
+        msg.reply(`Le message precedent a etait supprimer`);
+    } 
+});
+
+client.login("NzEwMDY4Mjc2MjcyNjkzMjY5.XrwOIw.unVb9ZUEnsRhYD0ktgOgLID78gQ");

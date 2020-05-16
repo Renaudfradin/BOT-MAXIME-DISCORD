bfe114a393550cd885256a407663767c193c8e04
Renaud <renaudfradin@gmail.com>
2020-05-16 23:48:38 +0200

    4

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ client.on("ready" , () => {
 });
 
 client.on("message" , msg => {
-    if (msg.content === `${PREFIX}hello`) {
-        msg.reply("BONJOUR MAITRE");
-    }else if (msg.content === `${PREFIX}batar`) {
+    if (msg.content === `${PREFIX}batar`) {
         msg.delete(msg);
         msg.reply(`Le message precedent a etait supprimer`);
     }else if(msg.content === `${PREFIX}help`){
-        msg.reply("`\n Voicie les commande du bot \n !hello = Bonjour maitre \n !batar = suprime le dernier message \n !bonjour = Bonjour au grand maitre quand nono a ecrit \n !help = affiche les commande du bot`");
-    }else if (msg.author.tag === "NONO70063#4609" && msg.content === `${PREFIX}bonjour`) {
+        msg.reply("`\n Voicie les commande du bot \n !hello = Bonjour au grand maitre quand nono parle et quand c'est une autre bonjour \n !batar = suprime le dernier message \n !help = affiche les commande du bot`");
+    }else if (msg.author.tag === "NONO70063#4609" && msg.content === `${PREFIX}hello`) {
         msg.reply(`Bonjour au grand maitre`);
-    }else if(msg.content === `${PREFIX}bonjour`){
-        msg.reply(`Tu n'est pa NONO`);
+    }else if(msg.content === `${PREFIX}hello`){
+        msg.reply(`Bonjour`);
     }
 });
 
